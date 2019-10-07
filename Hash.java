89c642bff8103c6f084b4c2c5b3fef524ea63c36
ricardomj0499 <ricardomj0499@hotmail.com>
2019-10-07 16:18:43 -0600

    probabilidades

---
+++ Hash.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caso8;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Hash {
+    
+    public int  FunctionHash(int R, int G, int B){
+        int indice;
+        indice = R+G+B;
+        
+        
+        return indice;
+    }
+    
+    
+    
+    
+    
+    public Hash(){
+        
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Genetic
-CasoOcho
+# Caso8
+Análisis de Algoritmos - Caso 8 - 2019
