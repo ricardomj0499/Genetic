09456d2dc02e531a76595049e666a8fae5217619
DAC125 <dcapro170_@hotmail.com>
2019-10-13 05:39:15 -0600

    creacion del cromosoma, seleccion, coordenas...

--- Caso8/src/caso8/Caso8.java
+++ Caso8/src/caso8/Caso8.java
@@ -2,8 +2,11 @@ package caso8;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import static javafx.scene.paint.Color.color;
 import static javafx.scene.paint.Color.color;
 import static javafx.scene.paint.Color.color;
@@ -23,29 +26,80 @@ public class Caso8 {
         todos los cuadrantes van a tener una probablilidad de uno
         */
         double[][] probabilidades = new double[8][8];
+        
         for(int i = 0;i <8; i++){
             for(int j = 0; j<8; j++){
                 probabilidades[i][j] = 1;
             }
         }
         
-        BufferedImage image = ImageIO.read(Metadata.class.getResource("Inkedverduras_LI.jpg"));//imagen a tomar
+        BufferedImage image = ImageIO.read(Metadata.class.getResource("stick.jpg"));//imagen a tomar   Inkedverduras_LI
+        BufferedImage imagen = new BufferedImage(1024,1024, TYPE_INT_RGB);
+        ArrayList<ColorList> coords_a_ordenar  = new ArrayList();//coords de cada cuadrante
         
-        for(int i = 0; i<5; i++){
-            image = Metadata.getting_metadata(image, probabilidades);
-        }
+        //for(int i = 0; i<5; i++){
+            imagen= Metadata.getting_metadata(image, probabilidades, coords_a_ordenar);
+                        //System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+        //}
             try {
                         // retrieve image
                         File outputfile = new File("newImage.jpg");
-                        ImageIO.write(image, "jpg", outputfile);
-                    } catch (IOException e) {
+                        ImageIO.write(imagen, "jpg", outputfile);
+                } catch (IOException e) {
                         e.printStackTrace();
-                    }    
+                } 
+            try {
+                        // retrieve image
+                        File outputfile = new File("newImage2.jpg");
+                        ImageIO.write(image, "jpg", outputfile);
+                } catch (IOException e) {
+                        e.printStackTrace();
+                } 
 
-        
+        //coords_a_ordenar = Metadata.getting_metadata(image, probabilidades, coords_a_ordenar);++++++++++++++++++++++++++++++++++
+            
         //Metadata FirstImage = new Metadata(image);
         //FirstImage.getting_metadata(image);
         
+       // acá se recorren las listas de xy de determindo color
+        int size=coords_a_ordenar.size();
+        
+        for(int x=0;x<coords_a_ordenar.size();x++) {
+            for(Coords coord: coords_a_ordenar.get(x).getRed()){
+                    System.out.println("++++++++++++++++++++++");
+                    System.out.println(coord.getX());
+                }
+            System.out.println("\n");
+            
+            /*coords_a_ordenar.get(x).getBlue().;
+            coords_a_ordenar.get(x).getCyan();
+            coords_a_ordenar.get(x).getGreen();
+            coords_a_ordenar.get(x).getMagenta();
+            coords_a_ordenar.get(x).getNegro();
+            coords_a_ordenar.get(x).getRed();
+            coords_a_ordenar.get(x).getYellow();*/
+        }
+        
+        NewCreateHTML HTML = new NewCreateHTML();
+        HTML.generarHTML();
+        
+        /*
+        Iterator<ColorList> nombreIterator = coords_a_ordenar.iterator();
+        while(nombreIterator.hasNext()){
+            ArrayList<ColorList> numeros = nombreIterator.next();
+            Iterator<Integer> itrNumeros = numeros.iterator();
+            while(itrNumeros.hasNext()){
+                    int numero = itrNumeros.next();
+                    if (numero == 0)
+                            itrNumeros.remove();
+            }
+        }*/
+        /*
+        for(Coords coord: coords_a_ordenar.getYellow()){
+                    System.out.println(coord.getY());
+        }*/
     }
+    
     
 }
---
+++ Caso8/src/caso8/Chromosome.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caso8;
+
+/**
+ *
+ * @author DAC_125
+ */
+public class Chromosome {
+
+    /*private String cromocolor;
+    private String cromocoord;
+    String cromosoma;
+    int color;
+    int cordenada;
+    Binary binario = new Binary();*/
+    Binary binario = new Binary();
+    int R;
+    int G;
+    int B;
+    String cromosoma;
+    private String cromocolor;
+    private String cromocoord;
+    
+    public Chromosome() {
+        
+    }
+
+   /* public String getCromosoma() {
+        return cromosoma;
+    }
+
+    private void setCromosoma(String cromocolor, String cromocoord) {
+        this.cromosoma = cromocolor+cromocoord;
+    }
+
+    private String getCromocolor() {
+        return cromocolor;
+    }
+
+    private void setCromocolor(String cromcolor) {
+        this.cromocolor = cromcolor;
+        setCromosoma(cromocolor, getCromocoord());
+    }
+
+    public String getCromocoord() {
+        return cromocoord;
+    }
+
+    private void setCromocoord(String cromcoord) {
+        this.cromocoord = cromcoord;
+        setCromosoma(getCromocolor(), cromocoord);
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+        String col = binario.obtenerBinario(color);
+        setCromocolor(col);
+    }
+
+    public int getCordenada() {
+        return cordenada;
+    }
+
+    public void setCordenada(int cordenada) {
+        this.cordenada = cordenada;
+        String cor = binario.obtenerBinario(cordenada);
+        setCromocoord(cor);
+    }
+*/
+    
+}
